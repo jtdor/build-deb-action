a12364dad258ad74a77466e20a0745b55e991600
Jörn-Thorben Hinz <jtdor@users.noreply.github.com>
2020-10-01 18:10:04 +0200

    Add a custom action to build a Debian package

---
+++ .github/actions/build-deb-package/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+
+# Calling `apt-get build-dep` with ./ here to easily keep compatibility with
+# old apt versions
+apt-get build-dep --no-install-recommends --yes ./
+
+dpkg-buildpackage "$@"
+
