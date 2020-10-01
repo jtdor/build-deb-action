3ecb30f3f6987abad93fe1134ab7f1f350452110
Jörn-Thorben Hinz <jtdor@users.noreply.github.com>
2020-10-01 22:28:00 +0200

    Be quiet, apt-get!

--- .github/actions/build-deb-package/entrypoint.sh
+++ .github/actions/build-deb-package/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 # Calling `apt-get build-dep` with ./ here to easily keep compatibility with
 # old apt versions
-apt-get build-dep --no-install-recommends --yes ./
+apt-get build-dep --no-install-recommends --quiet --yes ./
 
 dpkg-buildpackage "$@"
 
