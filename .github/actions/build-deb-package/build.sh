3351466ba2d522d7d371048b95b299ea1b77080a
Jörn-Thorben Hinz <jtdor@users.noreply.github.com>
2020-10-03 13:27:35 +0200

    Allow extra options to be passed to apt-get

--- .github/actions/build-deb-package/build.sh
+++ .github/actions/build-deb-package/build.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-DEBIAN_FRONTEND=noninteractive apt-get update --quiet
+DEBIAN_FRONTEND=noninteractive apt-get update --quiet $APT_ARGS
 
-DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends --quiet --yes dpkg-dev
+DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends --quiet --yes $APT_ARGS -- dpkg-dev
 
 # Calling `apt-get build-dep` with ./ here to easily keep compatibility with
 # old apt versions
-DEBIAN_FRONTEND=noninteractive apt-get build-dep --no-install-recommends --quiet --yes ./
+DEBIAN_FRONTEND=noninteractive apt-get build-dep --no-install-recommends --quiet --yes $APT_ARGS -- ./
 
 dpkg-buildpackage "$@"
 
