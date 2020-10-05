63133db8ca625ec92adc5ab657f7357c46a468b0
Jörn-Thorben Hinz <jtdor@users.noreply.github.com>
2020-10-05 11:21:42 +0200

    Prefix APT_ARGS with BDP_ for less ambiguity

--- .github/actions/build-deb-package/build.sh
+++ .github/actions/build-deb-package/build.sh
@@ -12,11 +12,11 @@ mv_other()
 
 DEBIAN_FRONTEND=noninteractive apt-get update --quiet $APT_OPTS
 
-DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends --quiet --yes $APT_OPTS -- dpkg-dev
+DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends --quiet --yes $BDP_APT_OPTS -- dpkg-dev
 
 # Calling `apt-get build-dep` with ./ here to easily keep compatibility with
 # old apt versions
-DEBIAN_FRONTEND=noninteractive apt-get build-dep --no-install-recommends --quiet --yes $APT_OPTS -- ./
+DEBIAN_FRONTEND=noninteractive apt-get build-dep --no-install-recommends --quiet --yes $BDP_APT_OPTS -- ./
 
 dpkg-buildpackage "$@"
 
