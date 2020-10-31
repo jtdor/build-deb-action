aa16e1f332fa993661d45058ed4166e75b6350f8
Jörn-Thorben Hinz <jtdor@users.noreply.github.com>
2020-10-31 13:30:29 +0100

    Break some long command lines

--- scripts/install_build_deps.sh
+++ scripts/install_build_deps.sh
@@ -8,6 +8,16 @@ apt-get update --quiet
 
 # Installing build-essential instead of dpkg-dev here. The former is designated
 # essential for package building by the Debian Policy.
-apt-get install --no-install-recommends --quiet --yes $INPUT_APT_OPTS -- build-essential $INPUT_EXTRA_BUILD_DEPS
+apt-get install \
+	--no-install-recommends \
+	--quiet \
+	--yes \
+	$INPUT_APT_OPTS \
+	-- build-essential $INPUT_EXTRA_BUILD_DEPS
 
-apt-get build-dep --no-install-recommends --quiet --yes $INPUT_APT_OPTS -- "$INPUT_SOURCE_DIR"
+apt-get build-dep \
+	--no-install-recommends \
+	--quiet \
+	--yes \
+	$INPUT_APT_OPTS \
+	-- "$INPUT_SOURCE_DIR"
