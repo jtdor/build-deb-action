a8a683f012a8ef79093aef5de0f264bbf83f9d72
Jörn-Thorben Hinz <jtdor@users.noreply.github.com>
2021-03-10 13:14:56 +0100

    Remove bdp_ prefix from functions

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,22 +3,22 @@
 set -eu
 
 # Usage:
-#   bdp_error MESSAGE
-bdp_error()
+#   error MESSAGE
+error()
 {
 	echo "::error::$1"
 }
 
 # Usage:
-#   bdp_end_group
-bdp_end_group()
+#   end_group
+end_group()
 {
 	echo "::endgroup::"
 }
 
 # Usage:
-#   bdp_start_group GROUP_NAME
-bdp_start_group()
+#   start_group GROUP_NAME
+start_group()
 {
 	echo "::group::$1"
 }
@@ -47,41 +47,41 @@ trap clean_up EXIT INT HUP TERM
 
 INPUT_ARTIFACTS_DIR=${INPUT_ARTIFACTS_DIR:-.}
 if ! check_path_prefix "$INPUT_ARTIFACTS_DIR" "$GITHUB_WORKSPACE"; then
-	bdp_error "artifacts-dir is not in GITHUB_WORKSPACE"
+	error "artifacts-dir is not in GITHUB_WORKSPACE"
 	exit 2
 fi
 
 INPUT_SOURCE_DIR=${INPUT_SOURCE_DIR:-.}
 if ! check_path_prefix "$INPUT_SOURCE_DIR" "$GITHUB_WORKSPACE"; then
-	bdp_error "source-dir is not in GITHUB_WORKSPACE"
+	error "source-dir is not in GITHUB_WORKSPACE"
 	exit 2
 fi
 
-bdp_start_group "Preparing build container"
+start_group "Preparing build container"
 env > "$env_file"
-docker run \
+container_id=$(docker run \
 	--detach \
 	--env-file="$env_file" \
-	--name=bdp_container \
 	--rm \
 	--volume="$GITHUB_ACTION_PATH":/github/action \
 	--volume="$GITHUB_WORKSPACE":/github/workspace \
 	--workdir=/github/workspace \
-	-- "$INPUT_DOCKER_IMAGE" tail -f /dev/null
-bdp_end_group
+	-- "$INPUT_DOCKER_IMAGE" tail -f /dev/null \
+)
+end_group
 
-bdp_start_group "Installing build dependencies"
-docker exec bdp_container /github/action/scripts/install_build_deps.sh
-bdp_end_group
+start_group "Installing build dependencies"
+docker exec "$container_id" /github/action/scripts/install_build_deps.sh
+end_group
 
-bdp_start_group "Building package"
-docker exec bdp_container /github/action/scripts/build_packages.sh
-bdp_end_group
+start_group "Building package"
+docker exec "$container_id" /github/action/scripts/build_packages.sh
+end_group
 
-bdp_start_group "Moving artifacts"
-docker exec bdp_container /github/action/scripts/move_artifacts.sh
-bdp_end_group
+start_group "Moving artifacts"
+docker exec "$container_id" /github/action/scripts/move_artifacts.sh
+end_group
 
-bdp_start_group "Stopping build container"
-docker stop --time=1 bdp_container
-bdp_end_group
+start_group "Stopping build container"
+docker stop --time=1 "$container_id" >/dev/null
+end_group
