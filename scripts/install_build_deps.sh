f4e99ffcab86129fe0bc71996748d9fba6031b1c
Jörn-Thorben Hinz <jtdor@users.noreply.github.com>
2021-03-10 19:33:20 +0100

    Go back to installing dpkg-dev instead of build-essential

--- scripts/install_build_deps.sh
+++ scripts/install_build_deps.sh
@@ -6,17 +6,18 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get update --quiet
 
-# Installing build-essential instead of dpkg-dev here. The former is designated
-# essential for package building by the Debian Policy.
-apt-get install \
-	--no-install-recommends \
-	--quiet \
-	--yes \
-	$INPUT_APT_OPTS \
-	-- build-essential $INPUT_EXTRA_BUILD_DEPS
-
 apt-get build-dep \
 	--quiet \
 	--yes \
 	$INPUT_APT_OPTS \
 	-- "./$INPUT_SOURCE_DIR"
+
+# In theory, explicitly installing dpkg-dev would not be necessary. `apt-get
+# build-dep` will *always* install build-essential which depends on dpkg-dev.
+# But let’s be explicit here.
+apt-get install \
+	--no-install-recommends \
+	--quiet \
+	--yes \
+	$INPUT_APT_OPTS \
+	-- dpkg-dev $INPUT_EXTRA_BUILD_DEPS
