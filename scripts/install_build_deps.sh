e687d0fc3fef6c0df80da11ccf0aea4baf46216b
Jörn-Thorben Hinz <jtdor@users.noreply.github.com>
2021-12-06 20:15:41 +0100

    Configure APT in /etc instead of repeatedly using the same command-line options

--- scripts/install_build_deps.sh
+++ scripts/install_build_deps.sh
@@ -2,20 +2,26 @@
 
 set -eu
 
+APT_CONF_FILE=/etc/apt/apt.conf.d/50build-deb-action
+
 export DEBIAN_FRONTEND=noninteractive
+
+cat > "$APT_CONF_FILE" <<-EOF
+	APT::Get::Assume-Yes "yes";
+	APT::Install-Recommends "no";
+	quiet "yes";
+EOF
 
 # Adapted from pbuilder's support for cross-compilation:
 if [ -n "$INPUT_HOST_ARCH" ]; then
 	dpkg --add-architecture "$INPUT_HOST_ARCH"
 	INPUT_EXTRA_BUILD_DEPS="$INPUT_EXTRA_BUILD_DEPS crossbuild-essential-$INPUT_HOST_ARCH libc-dev:$INPUT_HOST_ARCH"
+	printf 'APT::Get::Host-Architecture "%s";\n' "$INPUT_HOST_ARCH" >> "$APT_CONF_FILE"
 fi
 
-apt-get update --quiet
+apt-get update
 
 apt-get build-dep \
-	--quiet \
-	--yes \
-	${INPUT_HOST_ARCH:+--host-architecture="$INPUT_HOST_ARCH"} \
 	$INPUT_APT_OPTS \
 	-- "./$INPUT_SOURCE_DIR"
 
@@ -23,8 +29,5 @@ apt-get build-dep \
 # build-dep` will *always* install build-essential which depends on dpkg-dev.
 # But let’s be explicit here.
 apt-get install \
-	--no-install-recommends \
-	--quiet \
-	--yes \
 	$INPUT_APT_OPTS \
 	-- dpkg-dev $INPUT_EXTRA_BUILD_DEPS
