0774f2c12f21c8b2703ad1797446e1c759e9f549
Jörn-Thorben Hinz <jtdor@users.noreply.github.com>
2021-12-07 11:04:14 +0100

    Unwrap now shortened command lines

--- scripts/install_build_deps.sh
+++ scripts/install_build_deps.sh
@@ -21,13 +21,9 @@ fi
 
 apt-get update
 
-apt-get build-dep \
-	$INPUT_APT_OPTS \
-	-- "./$INPUT_SOURCE_DIR"
+apt-get build-dep $INPUT_APT_OPTS -- "./$INPUT_SOURCE_DIR"
 
 # In theory, explicitly installing dpkg-dev would not be necessary. `apt-get
 # build-dep` will *always* install build-essential which depends on dpkg-dev.
 # But let’s be explicit here.
-apt-get install \
-	$INPUT_APT_OPTS \
-	-- dpkg-dev $INPUT_EXTRA_BUILD_DEPS
+apt-get install $INPUT_APT_OPTS -- dpkg-dev $INPUT_EXTRA_BUILD_DEPS
